18496b83917cab19330c415b0beaae185490efa1
David Gavilanes <davidgavilanesvelez@gmail.com>
2022-05-22 14:43:26 +0200

    added header and views

---
+++ src/components/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="AppLayout">
+    <Nav />
+    <div>
+      <router-view />
+    </div>
+  </main>
+</template>
+
+<script>
+import Nav from '@/components/common/NavComponent.vue'
+
+export default {
+  components: { Nav }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import AppLayout from '@/components/layouts/AppLayout'
+import Cards from '@/views/CardsView'
+import Home from '@/views/HomeView'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'layout',
+    component: AppLayout,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: 'cards',
+        name: 'cards',
+        component: Cards
+      }
+    ]
   }
 ]
 
